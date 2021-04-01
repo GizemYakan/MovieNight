f69ac3614a9cfaf8a6d1d5caa0760362248d2368
GIZEM YAKAN <gizem.yakan9@gmail.com>
2021-04-01 14:07:51 +0300

    Add project files.

---
+++ MovieNight/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,37 @@
+﻿using MovieNight.Areas.Admin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieNight.Areas.Admin.Controllers
+{
+    public class DashboardController : AdminBaseController
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            //SQL : SELECT Year ,COUNT(*)AS Count 
+            //FROM Movies GROUP BY Year ORDER BY YEAR
+
+            List<MovieYearCount> movieYearCounts =
+                 db.Movies
+                 .GroupBy(x => x.Year)
+                 .Select(g => new MovieYearCount()
+                 {
+                     Year = g.Key,
+                     Count = g.Count()
+
+                 })
+                 .ToList();
+
+            DashboardViewModel vm = new DashboardViewModel()
+            {
+                MovieYearCounts = movieYearCounts
+            };
+
+            return View(vm);
+        }
+    }
+}
